d5ae5c75e02aa45e92bf2fb5713b61e51fb25354
Keshav <keshavmittal10767@gmail.com>
2024-06-28 17:13:47 +0530

    todo-list-react

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let myvar=335;
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <div>{myvar}</div>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
